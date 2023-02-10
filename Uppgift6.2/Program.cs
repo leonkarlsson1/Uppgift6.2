4fa2e6a117bba52eb3968f43eacb9d057689e9c3
Suberhaibe <hibba04@hotmail.se>
2023-02-10 11:10:41 +0100

    Add project files.

---
+++ Uppgift6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace uppgift_6_2
+
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("De två talen var 20 och 50, det största talet av dessa två står här under: ");
+
+            Console.WriteLine(Störst(20, 50));
+        }
+        static int Störst(int tal1, int tal2)
+
+        {
+
+            if (tal1 > tal2)
+
+            {
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+
+            }
+
+        }
+    }
+}
